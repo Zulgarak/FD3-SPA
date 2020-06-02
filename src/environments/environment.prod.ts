8e00b52582c57f17c70b6baa20a263a31b2b33df
Oleg Maksimchik <olegmr1991@gmail.com>
2020-06-02 19:18:16 +0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: true,
   api: 'https://fe3-angular.firebaseio.com',
+  apiKey: 'AIzaSyCtaGNCSCq_nzBHIzwiZ5nMbNdafLVpKDc',
+  signUpUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=',
+  loginUrl: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=',
+  authRedirectUrl: '/cars',
 };
